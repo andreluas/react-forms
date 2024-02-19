9e8c8d79d4ab9ca50db22bafa44c50a7ff5e6ba6
andreluas <andreluas.k@gmail.com>
2024-02-19 17:04:33 -0300

    using FormData for get inputs data

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,6 +9,8 @@ export default function Login() {
 
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
+
+    console.log(enteredEmail, enteredPassword);
   }
 
   return (
