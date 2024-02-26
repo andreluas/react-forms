daf93ada5b0aadc43576755e3be7c8c71fa1a892
andreluas <andreluas.k@gmail.com>
2024-02-26 09:59:10 -0300

    validating with refs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsValid, setEmailIsValid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
@@ -10,9 +12,14 @@ export default function Login() {
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
 
-    console.log(enteredEmail, enteredPassword);
-    email.current.value = "";
-    password.current.value = "";
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsValid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
   }
 
   return (
@@ -23,6 +30,9 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsValid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
